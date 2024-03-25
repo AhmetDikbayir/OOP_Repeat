b30ca360f7f896ca5c096b8b0f4371fe74db9bcb
Your Name <ahmet.dikbayir@gmail.com>
2024-03-26 00:23:53 +0300

    encapsulation package updated

--- src/oopConcepts/oop/inheritance/withInheritance/User.java
+++ src/oopConcepts/oop/inheritance/withInheritance/User.java
@@ -2,11 +2,11 @@ package oopConcepts.oop.inheritance.withInheritance;
 
 public class User {
 
-    private String name ;
-    private String surname ;
-    private String username ;
-    private String password ;
-    private String phoneNumber ;
+    public String name ;
+    public String surname ;
+    public String username ;
+    public String password ;
+    public String phoneNumber ;
 
     public void displayLessons(){
         System.out.println( name + " kisisinin tum dersleri ....");
